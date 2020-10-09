8e27982d509364fe62b7eff5db0bcee562e8f3c5
binarythistle <les.jackson@binarythistle.com>
2020-10-09 17:26:06 +1100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace changelogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
